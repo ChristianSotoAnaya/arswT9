4744d529afd91d249b71289ba536be3962e3575f
cristianmendivelso <cristian.mendivelso@mail.escuelaing.edu.co>
2017-03-15 18:45:28 -0500

    PARTE 2

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -5,11 +5,15 @@ function connect() {
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
         console.log('Connected: ' + frame);
-        
+
         stompClient.subscribe('/topic/newpoint', function (data) {
-            alert(data.body);
-            var theObject=JSON.parse(data.body);
-            
+            var theObject = JSON.parse(data.body);
+            var c = document.getElementById("myCanvas");
+            var ctx = c.getContext("2d");
+            ctx.beginPath();
+            ctx.arc(theObject.x, theObject.y, 1, 0, 2 * Math.PI);
+            ctx.stroke();
+
         });
     });
 }
@@ -26,11 +30,12 @@ function disconnect() {
 function myFunction() {
     x = prompt("Ingrese la coordenada x");
     y = prompt("Ingrese la coordenada y");
-    
-};
 
-function sendPoint(){
-    var p = new Punto(x,y);
+}
+;
+
+function sendPoint() {
+    var p = new Punto(x, y);
     console.log(JSON.stringify(p.coordenadas));
     stompClient.send("/topic/newpoint", {}, JSON.stringify(p.coordenadas));
 }
@@ -40,6 +45,23 @@ $(document).ready(
         function () {
             connect();
             console.info('connecting to websockets');
+            function getMousePos(canvas, evt) {
+                var rect = canvas.getBoundingClientRect();
+                return {
+                    x: evt.clientX - rect.left,
+                    y: evt.clientY - rect.top
+                };
+            }
+            var canvas = document.getElementById('myCanvas');
+            var context = canvas.getContext('2d');
+
+            canvas.addEventListener('mousedown', function (evt) {
+                var mousePos = getMousePos(canvas, evt);
+                var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
+                x=mousePos.x;
+                y=mousePos.y;
+                sendPoint();
+            }, false);
 
         }
 );
